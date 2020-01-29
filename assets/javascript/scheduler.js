899c65fe8496fad6e37deab3b73b8395e5f00520
Dane <56905214+dfmattern@users.noreply.github.com>
2020-01-29 00:19:34 -0700

    finished js code, centered text in table

--- assets/javascript/scheduler.js
+++ assets/javascript/scheduler.js
@@ -26,10 +26,10 @@ $(document).ready(function(){
         //console.log(this);
     
     //get add train values
-    let trainName = $("#trainName").val();
-    let destination =$("#destination").val(); 
-    let firstTrain = $("#first-train-time").val();
-    let frequency = $("#train-frequency").val();
+    let trainName = $("#trainName").val().trim();
+    let destination =$("#destination").val().trim(); 
+    let firstTrain = $("#first-train-time").val().trim();
+    let frequency = $("#train-frequency").val().trim();
 
     //push to database
     database.ref().push({
@@ -38,13 +38,76 @@ $(document).ready(function(){
         firstTrain : firstTrain,
         frequency : frequency
     });
+    alert("Train add Successful!")
 });
+
+
 database.ref().on("child_added", function(childSnapshot){
 
     let newTrain = childSnapshot.val().trainName;
     let newLocation = childSnapshot.val().destination;
     let newFirstTrain = childSnapshot.val().firstTrain;
     let newFrequency = childSnapshot.val().frequency;
+
+    let startTimeConvert = moment(newFirstTrain, "hh:mm").subtract(1, "years");
+    //console.log(startTimeConvert);
+
+    //current time
+    let currentTime = moment();
+    //console.log(currentTime);
+
+    //difference between current and first time
+    let timeDifference = moment().diff(moment(startTimeConvert), "minutes");
+    //console.log(timeDifference);
+
+    //time apart remainder
+    let remainder = timeDifference % newFrequency;
+    //console.log(remainder);
+
+    //minutes until next train
+    let arrivalMinutes = newFrequency-remainder;
+
+    //next arrival
+    let nextTrain = moment().add(arrivalMinutes, "minutes");
+    let nextTrainTime = moment(nextTrain).format("hh:mm");
+    console.log(nextTrainTime);
+
+    //apend new info to table
+    $("#table-body").append(
+        '<tr><td>' + newTrain +
+        '</td><td>'+ newLocation +
+        '</td><td>'+ newFrequency +
+        '</td><td>'+ nextTrainTime +
+        '</td><td>'+ arrivalMinutes + '</td></tr>');
+
+    //clear inputs
+    $("#trainName").val("");
+    $("#destination").val("");
+    $("#first-train-time").val("");
+    $("#train-frequency").val("");
+    return false;
+
+    //error handler
+    function errors (errorObject){
+        console.log("Errors handled: " + errorObject.code);
+        
+    }
+
+
+
+    
+    
+
+    
+    
+
+
+    
+
+
+    
+
+
 
 
 })
